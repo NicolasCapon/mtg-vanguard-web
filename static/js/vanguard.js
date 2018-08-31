ab53a7742ece18237b5a0ff5ce8a8aa208ea84aa
Nicolas CAPON <nicolascapon64@gmail.com>
2018-08-31 15:10:44 +0200

    card service

--- static/js/vanguard.js
+++ static/js/vanguard.js
@@ -2,11 +2,12 @@ $(function() {
     $("#generate").click(function(){
         $(this).prop("disabled",true);
         // if radio button, add value to form data
-        if ($( "#options" ).length) {
+        if ($( "#vanguard_image" ).attr("alt") == "Jhoira of the Ghitu") {
             var value = $("input[name='radio_type']:checked").val();
             var form = {"vanguard": $(this).attr('name'), "option": value};
-        } else{
-            var form = {"vanguard": $(this).attr('name')};
+        } else {
+            var value = $("#cmc_input").val();
+            var form = {"vanguard": $(this).attr('name'), "option": value};
         }
         $.ajax({
             url: '/getCard',
@@ -15,14 +16,23 @@ $(function() {
             dataType: 'json',
             success: function(response) {
                 cards = response.cards;
-                if(cards.length == 1){
+                if ($( "#vanguard_image" ).attr("alt") == "Jhoira of the Ghitu") {
+                    choose_card(cards);
+                } else if ($( "#vanguard_image").attr("alt") == "Maelstrom Archangel"){
                     image_url = cards[0].image_url;
                     name = cards[0].name;
-                    draw_image(image_url, name);
+                    create_choice_title();
+                    img = draw_image(image_url, name, $("#card_choice"));
+                    $(img).click(function(){
+                        draw_image(this.src, this.alt, $("#image_list"));
+                        $("#card_choice").empty();
+                    });
                 } else{
-                    choose_card(cards);
+                    image_url = cards[0].image_url;
+                    name = cards[0].name;
+                    create_choice_title();
+                    draw_image(image_url, name, $("#image_list"));
                 }
-
             },
             error: function(error) {
                 console.log(error);
@@ -31,40 +41,38 @@ $(function() {
     });
 });
 
-function draw_image(image_url, name){
+function create_choice_title(){
+    if(!$( "#h3_choice" ).length){
+        var h = document.createElement("h3");
+        h.innerHTML = "Choose a card";
+        h.setAttribute("id", "h3_choice");
+        $("#card_choice").prepend(h);
+    }
+}
+
+function draw_image(image_url, name, element){
     // Add image to image_list
     var img = document.createElement("img");
     img.src = image_url;
     img.alt = name;
     img.className = "classic_card";
-    $("#image_list").prepend(img);
+    element.append(img);
     $("#generate").prop("disabled",false);
+
+    return img;
 }
 
 function choose_card(images){
     // Create a div to choose one card to add to image_list
-    var div = document.createElement("div");
-    div.setAttribute("id", "card_choice");
-    div.setAttribute("class", "center");
-    var h = document.createElement("h3");
-    h.innerHTML = "Choose a card";
-
     for(var i=0; i < images.length; i++){
-        var img = document.createElement("img");
         image_url = images[i].image_url;
         name = images[i].name;
-        img.src = image_url;
-        img.alt = name;
-        img.className = "classic_card";
+        img = draw_image(image_url, name, $("#card_choice"));
         // On image click, add image to image_list
         $(img).click(function(){
-            draw_image(this.src, this.alt);
-            $("#card_choice").remove();
+            draw_image(this.src, this.alt, $("#image_list"));
+            $("#card_choice").empty();
             $("#generate").prop("disabled",false);
-            });
-        div.prepend(img);
+        });
     }
-    div.prepend(h);
-    $('#main').after(div);
 }
-
--- vanguard_model.py
+++ vanguard_model.py
@@ -3,10 +3,13 @@ import scryfallModel as scf
 
 
 def get_card_image_for_vanguard(vanguard, option):
-    """Return tuple(image_url, name) of a random card for Vanguards"""
-    print(option)
+    """Return tuple(image_url, name) of a random card for Vanguards
+       option arg for Jhoira and Maelstrom Archangel
+       num arg for maelstrom is the number of cards to returned"""
     cards = []
     if vanguard == "Momir Vig, Simic Visionary Avatar":
+        query = "t:creature+cmc:{}".format(option)
+        print(query)
         # cards = scf.search_cards(unique="cards", q="t:creature")
         # random_card = random.choice(cards)
         random_card = scf.get_random_card()
@@ -14,16 +17,32 @@ def get_card_image_for_vanguard(vanguard, option):
                       "image_url": scf.get_image_urls(random_card, size="normal")[0]})
 
     elif vanguard == "Jhoira of the Ghitu":
-        for i in [1, 2, 3]:
+        # option is instant or sorcery
+        query = "t:{}".format(option)
+        print(query)
+        for i in range(3):
+            # cards = scf.search_cards(unique="cards", q=query)
+            # random_card = random.choice(cards)
             random_card = scf.get_random_card()
             cards.append({"name": random_card["name"],
                           "image_url": scf.get_image_urls(random_card, size="normal")[0]})
 
-    elif vanguard == "stonehewer":
+    elif vanguard == "Stonehewer Giant":
+        query = "t:equipment+cmc<{}".format(option)
+        print(query)
+            # cards = scf.search_cards(unique="cards", q="t:equipment")
+            # random_card = random.choice(cards)
         random_card = scf.get_random_card()
         cards.append({"name": random_card["name"],
                       "image_url": scf.get_image_urls(random_card, size="normal")[0]})
-    else:
+
+    elif vanguard == "Maelstrom Archangel":
+        # Maelstrom Archangel
+        # option is int for ccm
+        query = "cmc:{}".format(option)
+        print(query)
+        # cards = scf.search_cards(unique="cards", q=query)
+        # random_card = random.choice(cards)
         random_card = scf.get_random_card()
         cards.append({"name": random_card["name"],
                       "image_url": scf.get_image_urls(random_card, size="normal")[0]})
